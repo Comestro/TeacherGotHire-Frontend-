8eb375c82a2628ededaec295cfe407288e67d3d4
rahulxdev0 <rk854327@gmail.com>
2024-11-22 16:14:12 +0530

    add school section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,8 @@ import RoleSelection from "../RoleSelection";
 import { useNavigate } from "react-router-dom";
 import { IoLocationOutline } from "react-icons/io5";
 import TeacherProfiles from "../Review/TeacherProfile";
+import TeacherSection from "../TeacherSection";
+import SchoolSection from "../SchoolSection";
 
 function Home() {
   const navigate = useNavigate();
@@ -130,47 +132,10 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="relative bg-teal-600 overflow-hidden mt-10">
-        {/* Header */}
-        {/* <div className="flex justify-between items-center px-6 py-4 bg-white shadow-md">
-        <button className="text-xl">
-          <span className="font-bold">☰</span>
-        </button>
-        <button className="text-sm font-medium text-blue-600 bg-blue-100 px-4 py-2 rounded-full shadow-md">
-          📞 Call Now (800) 803-4058
-        </button>
-      </div> */}
-
-        {/* Content */}
-        <div className="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 items-center justify-center px-6 sm:px-12 ">
-          {/* Left Section */}
-          <div className="text-white ml-20">
-            <h1 className="text-4xl sm:text-5xl font-bold mb-6 leading-loose">
-              क्या आप शिक्षक बनना चाहते हैं?
-            </h1>
-            <p className="text-lg sm:text-xl mb-6 leading-relaxed font-serif ">
-              क्या आप अपने शिक्षण करियर को नई दिशा देना चाहते हैं? आज ही आवेदन
-              करें और शिक्षा को बदलने के हमारे मिशन का हिस्सा बनें!
-            </p>
-            <button className="bg-white text-teal-600 font-medium text-lg px-6 py-3 rounded-full shadow-lg hover:bg-gray-100">
-              Explore More..
-            </button>
-          </div>
-
-          {/* Right Section */}
-          <div className="relative mt-10 text-center flex items-center justify-center">
-            <div className="w-64 h-64 lg:w-96 lg:h-96 mx-auto lg:mx-0">
-              <img
-                src="https://pngimg.com/d/teacher_PNG84.png"
-                alt="Classroom"
-                className="w-full h-full object-contain"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+      <TeacherSection onSelectRole={handleRoleSelection} />
       <TeacherProfiles />
-      <RoleSelection onSelectRole={handleRoleSelection} />
+      <SchoolSection onSelectRole={handleRoleSelection} />
+      {/* <RoleSelection onSelectRole={handleRoleSelection} /> */}
       <Footer />
     </div>
   );
