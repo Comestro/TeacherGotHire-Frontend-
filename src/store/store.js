40eba866dcd66348995e74251e016760a29f2da9
rahulxdev0 <rk854327@gmail.com>
2024-12-04 15:30:09 +0530

    exam portal work

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from '../features/authSlice';
 import personalProfileSlice from '../features/personalProfileSlice'
+import questionReducer from '../features/questionSlice';
 
 
 const store = configureStore({
     reducer:{
         auth : authSlice,
         personalProfile : personalProfileSlice,
+        questions : questionReducer,
     },
 })
 
