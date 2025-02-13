85d865eaf47c88c8d12e67279f644ad9d027f854
Archana-PC <archanamikki1998@gmail.com>
2025-02-13 16:31:45 +0530

    Fiexd Modal refersh and Education validation

--- src/components/Dashboard/components/Modal.jsx
+++ src/components/Dashboard/components/Modal.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 
 const Modal = ({ isOpen, onClose, title, children }) => {
-    if (!isOpen) return null;
-  return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 md:p-0">
-      <div className="bg-white rounded-lg p-6 min-w-[400px] overflow-y-scroll max-h-[90vh]">
-        <div className="flex justify-between items-center mb-4 ">
-          <h3 className="text-lg font-semibold">{title}</h3>
-          <button 
-            onClick={onClose}
-            className="text-gray-500 hover:text-gray-700"
-          >
-            ×
-          </button>
-        </div>
-        {children}
+  if (!isOpen) return null;
+return (
+  <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4 md:p-0">
+    <div className="bg-white rounded-lg p-6 min-w-[400px] overflow-y-scroll max-h-[90vh]">
+      <div className="flex justify-between items-center mb-4 ">
+        <h3 className="text-lg font-semibold">{title}</h3>
+        <button 
+          onClick={onClose}
+          className="text-gray-500 hover:text-gray-700"
+        >
+          ×
+        </button>
       </div>
+      {children}
     </div>
-  )
-}
+  </div>
+)
+};
 
-export default Modal
+export default Modal;
