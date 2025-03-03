c25102fdeea823d499425f6c4d8266933c694d75
PRINCE RAJ <rajkumar.prince2050@gmail.com>
2025-03-03 16:06:04 +0530

    question user page dynamic done

--- src/services/adminManageQuestionManager.js
+++ src/services/adminManageQuestionManager.js
@@ -1,10 +1,7 @@
 import apiService from "./apiService";
 
-const endpoint = "/api/admin/question";
+const endpoint = "/api/admin/assigneduser";
 
-export const getQuestions = () => apiService.getAll(endpoint);
-export const getQuestionById = (id) => apiService.getById(endpoint, id);
-export const createQuestion = (data) => apiService.create(endpoint, data);
-export const updateQuestion = (id, data) => apiService.update(endpoint, id, data);
-export const deleteQuestion = (id) => apiService.delete(endpoint, id);
-export const deleteAllQuestion = () => apiService.deleteAll(endpoint);
+export const getQuestionsManager = () => apiService.getAll(endpoint);
+export const adminManageAssignedUserManager = (data) =>
+  apiService.create(endpoint, data);
