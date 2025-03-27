1c19b9109ffacacba2f76fab8f61e248c8bca892
rahulxdev0 <rk854327@gmail.com>
2025-03-27 13:34:40 +0530

    recruiter pannel work done and adding title

--- src/components/Recruiter/RecruiterLayout.jsx
+++ src/components/Recruiter/RecruiterLayout.jsx
@@ -2,20 +2,26 @@ import React from "react";
 import TeacherRecruiterHeader from "./components/RecruiterHeader";
 import { Outlet } from "react-router-dom";
 import RecruiterSidebar from "./components/RecruiterSidebar";
+import { Helmet } from "react-helmet-async";
 
 const RecruiterLayout = () => {
   return (
-    <div className='min:h-screen w-full"'>
-      <TeacherRecruiterHeader />
-      <div className="flex w-full">
-        <div className="hidden md:block">
-          <RecruiterSidebar />
-        </div>
-        <div className="w-full md:ml-72 mt-16">
-          <Outlet />
+    <>
+      <Helmet>
+        <title>PTPI | Recruiter Panel</title>
+      </Helmet>
+      <div className='min:h-screen w-full"'>
+        <TeacherRecruiterHeader />
+        <div className="flex w-full">
+          <div className="hidden md:block">
+            <RecruiterSidebar />
+          </div>
+          <div className="w-full md:ml-72 mt-16">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
